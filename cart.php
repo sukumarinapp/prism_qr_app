26231eb0e532c9492f5d2a910cd4222527599447
sukumarinapp <sukumar.inapp@gmail.com>
2021-12-17 14:04:43 +0530

    add database

--- cart.php
+++ cart.php
@@ -258,8 +258,6 @@ while($row = mysqli_fetch_array($result)){
 <script type="text/javascript" src="js/popper.min.js"></script>
 <!-- Bootstrap core JavaScript -->
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
-<!-- MDB core JavaScript -->
-<script type="text/javascript" src="js/mdb.min.js"></script>
 <script type="text/javascript">
   function print_order(){
     var salesprint = new Array();
