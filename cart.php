053e31b50306b24b31141e30da6842f1bf45ccfc
sukumarinapp <sukumar.inapp@gmail.com>
2021-12-17 14:12:43 +0530

    add database

--- cart.php
+++ cart.php
@@ -60,8 +60,6 @@ while($row = mysqli_fetch_array($result)){
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <!-- Bootstrap core CSS -->
   <link href="css/bootstrap.min.css" rel="stylesheet">
-  <!-- Material Design Bootstrap -->
-  <link href="css/mdb.min.css" rel="stylesheet">
   <!-- Your custom styles (optional) -->
   <link href="css/style.css" rel="stylesheet">
   <style type="text/css">
