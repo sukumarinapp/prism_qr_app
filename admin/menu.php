2fda2502268b92808ee8fca1b9ee3685833d02b5
universejino <universejino@gmail.com>
2021-12-20 17:38:40 +0530

    login

--- admin/menu.php
+++ admin/menu.php
@@ -11,16 +11,9 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class=" nav-item menu-open">
-            <a href="dashboard.php" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-              </p>
-            </a>
-          </li> 
+          
           <li class="nav-item">
-            <a href="order.php" class="nav-link">
+            <a href="dashboard.php" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Orders
