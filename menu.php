02f1da8027a6a3b79a8107459a9360f17f9e19bf
universejino <universejino@gmail.com>
2022-01-10 13:13:47 +0530

    login

--- menu.php
+++ menu.php
@@ -223,7 +223,7 @@ while($row = mysqli_fetch_array($result)){
         <div class="modal-dialog">
           <div class="modal-content" style="background-color: white;">
             <div class="modal-body" id="modalbody"></div>
-            <div class="text-center" style="text-align: center !important">
+            <div class="text-center" style="text-align: center !important;padding-bottom: 10px;">
               <button type="button" onclick="cancel_order()" style="background-color: #c55c58" class="btn btn-sm text-center" >Cancel</button>
               <button id="confirm_button" type="button" onclick="confirm_order()" style="background-color: #18b1b1" class="btn  btn-sm text-center" >Confirm</button>
             </div>
