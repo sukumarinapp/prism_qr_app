cc227018d63916f9bc12330c2a3d94cd86b51153
universejino <universejino@gmail.com>
2022-01-12 14:17:07 +0530

    login

--- admin/settings.php
+++ admin/settings.php
@@ -39,8 +39,8 @@ while($row = mysqli_fetch_array($result)){
               <form method="post" action="" >
                 <div class="card-body">
                   <div class="form-group">
-                    <label for="LANIPA">Web Service URL</label>
-                    <input value="<?php echo $LANIPA; ?>" required="required" type="text" name="LANIPA" class="form-control" id="LANIPA" placeholder="Web Service URL">
+                    <label for="LANIPA">Web Service IP:port</label>
+                    <input value="<?php echo $LANIPA; ?>" required="required" type="text" name="LANIPA" class="form-control" id="LANIPA" placeholder="eg : 192.168.1.98 : 98">
                   </div>
 
                   <div class="card-footer text-center">
