c31862c96a8412d0a95f4022b5859a3348280fcf
sukumarinapp <sukumar.inapp@gmail.com>
2022-02-09 15:29:04 +0530

    new repo

---
+++ admin/demo.sql
@@ -0,0 +1,15 @@
+PRISM QR App Demo URL and Login Credentials
+
+Ordering App
+https://prismblr.com/kot
+
+Dashboard
+https://prismblr.com/kot/admin/index.php?id=7
+
+Steward Login
+Username: AJAY
+Password: prism
+
+Supervisor Login
+Username: PRISM
+Password: prism
